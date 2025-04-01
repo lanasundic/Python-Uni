e320f06e0cfa3735d108f376eec99b79aa165f9a
Lana Sundic <lanasundic@gmail.com>
2025-04-01 19:38:51 +0200

    add changes

--- Lesson 03/liste.py
+++ Lesson 03/liste.py
@@ -6,11 +6,11 @@ lista3 = [2, 3.5, ]
 
 print(lista1[0])
 print(lista2[2])
-print(lista2[-1])
+print(lista2[-1])   #printuje samo posljednji karakter(Ana)
 
-print(lista1[1:4])
-print(lista1[1:4:2])
-print(lista1[-1:-2:-1])
+print(lista1[1:4])  #ukljucuje od a ne do - stampa 2 3 4
+print(lista1[1:4:2])    #od pocetka se gleda ovo svaki drugi
+print(lista1[-1:-2:-1]) #opet ukljucuje od a ne do - stampa 5
 
 lista1[0] = 10
 print(lista1)
@@ -27,16 +27,16 @@ print(lista1)
 #FUNKCIJE
 print(len(lista1))   #govori koliko ima lista elemenata
 
-lista1.append(12)  #dodaje jedan element na kraj liste
+lista1.append(12)  #dodaje jedan element na kraju liste
 print(lista1)
 
 lista1.insert(3, 20)  #na indeksu 3 ce da bude 20
 print(lista1)
 
-lista1.extend([2, 4, 6, 8])  #nadovezuje citavu listu jednu na drugu, odnosno dodsaje ove lemente n akraju liste
+lista1.extend([2, 4, 6, 8])  #nadovezuje citavu listu jednu na drugu, odnosno dodaje ove elemente na kraju liste
 print(lista1)
 
-lista1.append([2, 4, 6 ,8])
+lista1.append([2, 4, 6 ,8]) #stampa - [2, 4, 6, 8] sa sve zagradama, jer append prima samo 1 element
 print(lista1)
 
 lista1 = lista1 + [10, 12, 14]  # + je isto sto i ekstend samo sto ce se napravit nova lista
@@ -48,7 +48,7 @@ print(lista1)
 lista1.clear()  #sve obrise - dobijamo praznu listu
 print(lista1)
 
-lista1.pop()  #moze da se pozove sa jednim arg ili bez aegumenata, kada je bez obrisace poslednji el u nizu
+lista1.pop()  #moze da se pozove sa jednim arg ili bez argumenata, kada je bez obrisace poslednji el u nizu
 print(lista1)
 
 a = lista1.pop() # sa a je ako hocemo da vidimo sta smo obrisali
@@ -57,7 +57,7 @@ print(lista1)
 
 lista1.pop(4)  #cetvrti element ce da bude obrisan
 
-b = lista1.pop(4)
+b = lista1.pop(4)   #u b cuvamo samo obrisani element i on ce da se stampa
 print(b)
 print(lista1)
 
@@ -67,7 +67,7 @@ print(lista1)
 c = lista1.count(10)   #broji koliko se neki element javlja 
 print(c)
 
-d = lista1.index(4, 7, 9)  #nalazi index prvog pojavljivanja trazene vrijednosti u stringu, pocni od 7. indexa, zavrsi na 9.
+d = lista1.index(4, 7, 9)  #.index(x, start, end) pronalazi prvi indeks na kojem se nalazi vrijednost x u listi između indeksa start i end-1.
 print(d)
 
 print(14 in lista1)
@@ -79,11 +79,11 @@ print("rar" in "programiranje")  #recice false
 del lista1[3:5]  #del = delete, 3 do 5
 print(lista1)
 
-del lista1[2]
+del lista1[2]   #brise samo sl na index-u 2
 print(lista1)
 
 s1 = "danas ucimo o listama i drugim strukturama podataka"
-l1 = s1.split()  #od ovog stringa ce da napravi listu tako sto je od svakog blanka da napravi novi element
+l1 = s1.split()  #string "danas ucimo o listama i drugim strukturama podataka" je podijeljen na listu riječi, gdje je svaki element lista pojedinačna riječ. - Svaka reč iz stringa postaje zaseban element liste.
 print(l1)
 
 s2 = "7/3/2015"
@@ -96,14 +96,22 @@ print(l2)
 
 #kad imamo listu ali zelimo da je spojimo u string = suprotno od split
 l4 = ["pmf", "d", "smjer", "python"]
-s4 = " ".join(l4)  #" " i "" nije isto - bez blanka dobijamo pmfdsmjerpython 
+s4 = " ".join(l4)  #" " i "" nije isto - bez blanka dobijamo pmfdsmjerpython ---> "elemente liste spoji razmakom"
 print(s4)
+
+listaBrojeva = ["1", "2", "3", "4"]
+rezultat = "-".join(listaBrojeva)   # "elemente listespoji crticom -"
+print(rezultat)     #stampa 1-2-3-4
 
 l5 = ["7", "3", "2025"]
 s5 = "/".join(l5)
 print(s5)
 
-#DOMACI(bio na proslom kolokvijumu dje smo mi kucali kodove): Da korisnik unese datum i mi uzmemo tah datum i 
+#join() može da radi samo sa stringovima! Ako lista sadrži brojeve (int), prvo ih treba pretvoriti u stringove:
+brojevi = [1, 2, 3, 4]
+rezultat = "-".join
+
+#DOMACI(bio na proslom kolokvijumu dje smo mi kucali kodove): Da korisnik unese datum i mi uzmemo taj datum i 
 # odstampamo koji ce datum da bude nakon nekih k dana koje korisnik unese
 
 print(lista1)
@@ -123,18 +131,19 @@ print(lista1)
 lista2.sort(reverse = True)
 print(lista2)
 
-#sa key mozemo da zadamo nas nacim kako zelimo da sortiramo
+#sa key mozemo da zadamo nas nacin kako zelimo da sortiramo
 def fun1(x):
     return len(x)  #preslikali smo string u broj a taj broj je duzina od stringa
                     #po duzini ce da sortira
 lista2.extend(["Aleksandar", "Masa"])
 lista2.sort(key = fun1)
 print(lista2)
+#key = fun1 znači da se sortiranje vrši prema dužini stringova (pošto fun1 vraća len(x)).
 
 def fun2(x):
     return x%5  #sortira brojeve koji imaju ostatak 5 od najmanjeg do najveceg
 
-lista1.sort(key = fun2)
+lista1.sort(key = fun2) #zasto ovo nece?
 print(lista1)
 
 def fun3(x):
@@ -149,7 +158,7 @@ print(lista1)
 
 #FOR PETLJA:
 for x in lista1:
-    print(x)  #petljakoja prodje listom1 i odstampa sve elemente
+    print(x)  #petlja koja prodje listom1 i odstampa sve elemente
     
 #for + promjenljiva + in + kolekcija koja nas zanima
 
--- Lesson 03/opadajuci.py
+++ Lesson 03/opadajuci.py
@@ -1,10 +1,10 @@
 #da korisnik unese broj i da mi provjerimo da li su brojevi u opadajucem poretku
 
-broj = int(input("Unesite broj:"))
-s = str(broj)
-l = list(s)
-l.sort(reverse = True)
-t = "".join(l)
-print(s==t)
+broj = int(input("Unesite broj:"))  #koristnik unosi broj - npr. 54321
+s = str(broj)   #pretvaramo broj u string kako bismo mogli obraditi svaku cifru - "54321"
+l = list(s)     #pretvaramo string u listu karaktera - l = ['5', '4', '3', '2', '1']
+l.sort(reverse = True)  #sortiramo listu u opadajucem poretku
+t = "".join(l)      #spajamo sortirane cifre nazad u string
+print(s==t)     #uporedjujemo originalni broj i sortiranu verziju
 
-#pretvorimo bbroj u string string u lustu joinujemo...
+#pretvorimo broj u string, string u listu joinamo...
